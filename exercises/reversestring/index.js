84cad5284413ed59ff1b8e32e0872f5588757e06
Alexander Sierra <alexandercsierra@gmail.com>
2020-08-05 12:40:39 -0400

    completed reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,17 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse1(str) {
+    let reversed = str.split("").reverse().join("");
+    return reversed;
+}
+
+function reverse(str) {
+    newStr = "";
+    for (let i=str.length-1; i>=0; i--){
+        newStr+=str[i]
+    }
+    return newStr
+}
 
 module.exports = reverse;
