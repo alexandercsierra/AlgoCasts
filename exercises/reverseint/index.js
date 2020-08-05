b530ef15cce5b93cfe3f6391030fd6f1eda41e7c
Alexander Sierra <alexandercsierra@gmail.com>
2020-08-05 13:08:27 -0400

    completed reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,23 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let numArr = n.toString().split("").reverse();
+    
+    if(n < 0){
+        numArr.pop();
+        return Number(numArr.join("")) * -1;
+    } else {
+        return Number(numArr.join(""));
+    }
+
+}
+
+//Math.sign determines if the number is positive or negative and returns 1 or -1 accordingly
+function reverseInt2(n) {
+    let numArr = n.toString().split("").reverse().join("");
+    
+    return parseInt(numArr) * Math.sign(n)
+}
 
 module.exports = reverseInt;
