6546e022c1fdded8967dc33958dd40d3b8a801d7
Alexander Sierra <alexandercsierra@gmail.com>
2020-08-05 12:52:00 -0400

    completed palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,19 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome1(str) {
+    return str === str.split("").reverse().join("");
+}
+
+function palindrome(str) {
+    return str === str.split("").reduce((rev, char)=>char+rev,"")
+}
+
+//every executes the callback with every element of an array, returning a boolean - false if any one check is false, and true if all the checks returned true. Not ideal here, since you would only really need to check up until the midpoint of the string.
+function palindrome2(str){
+    return str.split('').every((char, i)=>{
+        return char === str[str.length-i-1];
+    })
+}
 
 module.exports = palindrome;
