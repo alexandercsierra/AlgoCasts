49b4087ff361b7b4b72a4ba90c7c6129a2ea91d2
Alexander Sierra <alexandercsierra@gmail.com>
2020-08-05 13:17:32 -0400

    completed maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    let freq = {}
+    let greatestNum = 0;
+    let greatestChar = "";
+
+    for(let char of str){
+        if(freq[char]){
+            freq[char] +=1;
+        } else{
+            freq[char] = 1;
+        }
+
+        if (freq[char] > greatestNum){
+            greatestNum = freq[char];
+            greatestChar = char;
+        }
+    }
+
+    return greatestChar;
+
+
+}
 
 module.exports = maxChar;
