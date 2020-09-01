3ab485ef2f4fd94aa66a0710d4a936c7c96f3e18
Oliver17! <alexandercsierra@gmail.com>
2020-09-01 19:09:14 -0400

    circular complete

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,31 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+    if(!list.head){
+        return false;
+    }
+    else if(!list.head.next){
+        return false
+    }
+
+    let cache = [list.head]
+    let current = list.head;
+
+    while(current.next && !cache.includes(current.next)){
+        cache.push(current.next);
+        current = current.next
+    }
+
+    if(current.next === null){
+        return false
+    } else{
+        return true
+    }
+
+
+
+
+}
 
 module.exports = circular;
